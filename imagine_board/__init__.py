2699653075ebc67740343b03b02f2e24767e1580
EyeOdin <59078314+EyeOdin@users.noreply.github.com>
2023-01-15 04:30:10 +0000

    Add files via upload

---
+++ imagine_board/__init__.py
@@ -0,0 +1,35 @@
+# Imagine Board is a Krita plugin to displays and organizes images.
+# Copyright (C) 2022  Ricardo Jeremias.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+# Imports
+from krita import *
+from .imagine_board_extension import *
+from .imagine_board_docker import *
+
+
+# Information
+__version__ = ' 1.0.0 '
+__license__ = ' GPLv3+ LGPLv3+ '
+__author__ = ' Ricardo Jeremias '
+__email__ = ' jeremy6321478@gmail.com '
+__url__ = ' https://github.com/EyeOdin '
+
+# Name the Python Script for the program
+DOCKER_ID = "pykrita_imagine_board_docker"
+
+# Register Krita Docker
+Application.addDockWidgetFactory( DockWidgetFactory(DOCKER_ID, DockWidgetFactoryBase.DockRight, ImagineBoard_Docker) )
